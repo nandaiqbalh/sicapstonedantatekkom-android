3c0cdeae8520f1eb95922f0240fb22f0fe1c301a
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2023-12-30 10:47:36 +0700

    First ini

---
+++ app/src/main/java/com/kel022322/sicapstonedantatekkom/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kel022322.sicapstonedantatekkom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
